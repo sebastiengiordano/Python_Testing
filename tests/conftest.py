9f6b48a3ed2bd1060d0696ddf7362623889ffc0d
Sébastien Giordano <sebastiengiordano@yahoo.fr>
2022-01-31 15:21:37 +0100

    Update test__status_code__ok

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime, timedelta
 
 from server import app
 
@@ -38,48 +39,13 @@ def clubs__data_for_test(monkeypatch):
 @pytest.fixture(scope='function')
 def competitions__data_for_test(monkeypatch):
     '''Used to modify the competitions dictionary for testing purposes.'''
+    today = datetime.today()
+    date_in_past = (today - timedelta(minutes=1))
+    date_in_past = date_in_past.strftime("%Y-%m-%d %H:%M:%S")
+    date_in_future = (today + timedelta(minutes=1))
+    date_in_future = date_in_future.strftime("%Y-%m-%d %H:%M:%S")
+
     data_for_test = [
-            {
-                "name": "Spring Festival",
-                "date": "2020-03-27 10:00:00",
-                "numberOfPlaces": "25"
-            },
-            {
-                "name": "Fall Classic",
-                "date": "2020-10-22 13:30:00",
-                "numberOfPlaces": "13"
-            }]
-    monkeypatch.setattr('server.competitions', data_for_test)
-
-
-@pytest.fixture(scope='function')
-def loadClubs_fixture(monkeypatch):
-    '''Used to modify the club dictionary for testing purposes.'''
-    def clubs__update():
-        return [
-            {
-                "name": "Testuser1 One",
-                "email": "email@testuser1.com",
-                "points": "13"
-            },
-            {
-                "name": "Testuser2 Two",
-                "email": "email@testuser2.fr",
-                "points": "4"
-            },
-            {
-                "name": "Testuser3 Three",
-                "email": "email@testuser3.uk",
-                "points": "12"
-            }]
-    monkeypatch.setattr('server.loadClubs', clubs__update)
-
-
-@pytest.fixture(scope='function')
-def loadCompetitions_fixture(monkeypatch):
-    '''Used to modify the competitions dictionary for testing purposes.'''
-    def competitions__update():
-        return [
             {
                 "name": "Test Festival",
                 "date": "2022-03-27 10:00:00",
@@ -89,5 +55,20 @@ def loadCompetitions_fixture(monkeypatch):
                 "name": "Test Classic",
                 "date": "2021-10-22 13:30:00",
                 "numberOfPlaces": "13"
+            },
+            {
+                "name": "Test Low Number of place",
+                "date": "2023-10-22 13:30:00",
+                "numberOfPlaces": "1"
+            },
+            {
+                "name": "Competition in past",
+                date_in_past,
+                "numberOfPlaces": "20"
+            },
+            {
+                "name": "Competition in future",
+                date_in_future,
+                "numberOfPlaces": "15"
             }]
-    monkeypatch.setattr('server.loadCompetitions', competitions__update)
+    monkeypatch.setattr('server.competitions', data_for_test)
